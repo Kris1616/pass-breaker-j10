32f1efe9d85c472d1c52708c84e5c625fe965d85
Krzysiek <nip@poczta.onet.pl>
2019-08-28 11:55:46 +0200

    Kolejna zmiana

--- src/main/java/app/PassBreaker.java
+++ src/main/java/app/PassBreaker.java
@@ -1,6 +1,35 @@
 package app;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 public class PassBreaker {
 
+    public void passBreaker(String password){
+        String pass = null;
+        boolean flag = true;
+        BigInteger counter = BigInteger.ZERO;
+        long start = System.nanoTime();
+        while(flag){
+            pass = RandomStringUtils.randomAlphabetic(password.length());
+            counter = counter.add(BigInteger.ONE);
+            if (pass.equalsIgnoreCase(password)){
+                break;
+            }
+            System.out.println(pass + " | " + counter);
+            try {
+                Thread.sleep(200);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        long stop = System.nanoTime();
+        double elapsedTime = (double) (stop - start)/1_000_000_000;
+        double convert = TimeUnit.MILLISECONDS.convert((start - stop), TimeUnit.NANOSECONDS);
+        System.out.println("BROKEN" + counter + " | " + pass + "at time sec: " + elapsedTime);
+    }
 
 }
